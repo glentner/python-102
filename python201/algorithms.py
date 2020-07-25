50b04cf6fdac82821d8b87704bafc58849f2d37a
Geoffrey Lentner <glentner@purdue.edu>
2020-07-25 18:58:45 -0400

    fix docstring and add typing to algorithms.py

--- python201/algorithms.py
+++ python201/algorithms.py
@@ -1,23 +1,18 @@
+from typing import List
 
-def cumulative_product(array):
+def cumulative_product(array: List[float]) -> List[float]:
     """
     Compute the cumulative product of an array of numbers.
 
-    Parameters
-    ----------
-    array : list (of numbers)
-        An array (list) of numeric values to compute the cumulative
-        product for.
+    Parameters:
+        array (list): An array of numeric values.
 
-    Returns
-    -------
-    result : list (of numbers)
-        A list of the same shape as `array`.
+    Returns:
+        result (list): A list of the same shape as `array`.
 
-    Example
-    -------
-    >>> cumulative_product([1, 2, 3, 4, 5])
-    [1, 2, 6, 24, 120]
+    Example:
+        >>> cumulative_product([1, 2, 3, 4, 5])
+        [1, 2, 6, 24, 120]
     """
     result = list(array).copy()
     for i, value in enumerate(array[1:]):
